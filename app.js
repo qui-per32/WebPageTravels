3529c872803536b63a21ac11fe15ddb376ed44e0
Enrique <qui.per.gonz32@gmail.com>
2018-05-30 13:42:34 +0200

    added bower, bootstrap and hbs with bootstrap.

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let hbs = require('hbs');
+let hbsUtils = require('hbs-utils') (hbs);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine partials
+hbsUtils.registerPartials(`${__dirname}/views/partials`);
+hbsUtils.registerWatchedPartials(`${__dirname}/views/partials`);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/components",express.static(`${__dirname}/public/components`));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
